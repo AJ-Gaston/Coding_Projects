e226e18ccbc1e7b4ac9ef6ce9e579ace59ca0301
Alexji Gaston <agast002@odu.edu>
2025-06-04 14:13:38 -0400

    'working on simple calculator'

--- C++/simple_calculator.cpp
+++ C++/simple_calculator.cpp
@@ -1,6 +1,26 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 
-void main(){
+int addition(){
+    int sum = 0;
+    return sum;
+}
 
+int subtraction(){
+    int sum = 0;
+    return sum;
+}
+
+int division(){
+    int sum = 0;
+    return sum;
+}
+
+int main(){
+    std::cout << "WELCOME TO THE CALCULATOR PROGRAM!\n";
+    std::cout << "What would you like to do?";
+    
+
+    return 0;
 }
