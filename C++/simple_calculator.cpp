f9401a92c941952bc336258339075c0f5cb19f3d
Alexji Gaston <agast002@odu.edu>
2025-07-22 17:28:47 -0400

    'working on menu'

--- C++/simple_calculator.cpp
+++ C++/simple_calculator.cpp
@@ -1,26 +1,50 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
 
-int addition(){
-    int sum = 0;
+int addition(int num1, int num2){
+    int sum = num1+ num2;
     return sum;
 }
 
-int subtraction(){
-    int sum = 0;
+int subtraction(int num1, int num2){
+    int sum = num1-num2;
     return sum;
 }
 
-int division(){
-    int sum = 0;
+int division(int num1, int num2){
+    if (num1 == 0|| num2 == 0){
+
+    }
+    int sum = num1/num2;
+    
     return sum;
+}
+void displayMenu()
+{
+    std::cout << "==== CALCULATOR MAIN MENU ====";
+    std::cout << "1. Addition";
+    std::cout << "2. Subtraction";
+    std::cout << "3. Multiplication";
+    std::cout << "4. Division";
+    std::cout << "5. Quit";
 }
 
 int main(){
-    std::cout << "WELCOME TO THE CALCULATOR PROGRAM!\n";
-    std::cout << "What would you like to do?";
-    
+    int choice;
+    while (true){
+        displayMenu();      //Display the menu for user's choice
+        std::cin >> choice;
+        switch(choice){
+            case 1:
+            std::cout << "You have chosen Addition!\n";
+            case 2:
+            case 3:
+            case 4:
 
-    return 0;
+        }
+    }
+    
+    
 }
